2c9b1382364540cbc3d414fe5749993c183ff90e
sosnus <sosnus15@gmail.com>
2020-07-14 22:52:29 +0200

    sms-node-emulator simple project

---
+++ sms-node-emulator/app.py
@@ -0,0 +1,35 @@
+import requests
+import random
+import time
+
+
+print("HI11111!")
+time.sleep(5)
+print("Wait for other containers...")
+
+
+def writeToInfluxDb( db, device, value):
+    # print(random.randint(3, 9)) 
+    url = "http://sms-influxdb-container:8086/write?db="+db
+    # url = "http://172.21.0.3:8086/write?db="+db
+
+    payload = "temperature,device="+ device +" value="+str(value)
+    #  + string(random.randint(3, 9))"
+    headers = {
+    'Content-Type': 'text/plain'
+    }
+
+    response = requests.request("POST", url, headers=headers, data = payload)
+
+    print(payload)
+    print(response.text.encode('utf8'))
+
+
+
+while True:
+    print("new loop!")
+    writeToInfluxDb("greenhouse_jaroszki", "node02", random.randint(15,30))
+    writeToInfluxDb("greenhouse_jaroszki", "node03", random.randint(5,10))
+    writeToInfluxDb("greenhouse_jaroszki", "node04", random.randint(5,10))
+    writeToInfluxDb("greenhouse_jaroszki", "outdoor", random.randint(5,30))
+    time.sleep(5)
