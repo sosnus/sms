1376a2ea72e3c6ac63d43e7a4b43b92de3bb1dc7
sosnus <sosnus15@gmail.com>
2020-07-28 03:30:42 +0200

    prepare new docker project with mqtt to influxdb parser

--- sms-scripts/build-containers.sh
+++ sms-scripts/build-containers.sh
@@ -1,6 +1,10 @@
 docker rm sms-node-emulator-container
 docker build -t sms-node-emulator ../sms-node-emulator/
 
+docker rm sms-app
+docker build -t sms-appr ../sms-app/
+
 docker run -d --name=sms-influxdb-container --network sms-net -v v-sms-influx:/var/lib/influxdb influxdb:1.7
 docker run -d -p 3000:3000 --name=sms-grafana-container --network sms-net -v v-sms-grafana:/var/lib/grafana grafana/grafana
 docker run -d --name=sms-node-emulator-container --network sms-net sms-node-emulator
+docker run -d --name=sms-app-container --network sms-net sms-app
