0ac18b2d948ac2ec4ba1c60c7d957185dc6fa7b9
Ubuntu <sms-admin@sms-pallene.s0kf3j1octjedkiexudbontl3a.ax.internal.cloudapp.net>
2020-07-28 07:40:09 +0000

    try new type of requests

--- sms-app-paho/app-divide-to-messages-without-http-req-05.py
+++ sms-app-paho/app-divide-to-messages-without-http-req-05.py
@@ -1,10 +1,7 @@
-import requests
-import time
 import json
 import paho.mqtt.client as mqtt
 
 # todo: secrets, method parameters, http requests, docker, logger
-# requests: https://requests.readthedocs.io/en/master/user/quickstart/#more-complicated-post-requests
 
 localhost = 'eu.thethings.network'
 port = 1883
@@ -14,18 +11,10 @@ topic = '+/devices/+/up'
 
 def mqtt_to_http(node_name, node_payload_fields, node_db = 'orchard_jaroszki', node_time=-1):
   print('node_db=', node_db,  ' node_name=', node_name, ' node_payloads=', node_payload_fields, ' time=', node_time)
-  # print("body parse")
-#  print(type( node_payload_fields))
+  print("body parse")
+  print(type( node_payload_fields))
   for k,v in  node_payload_fields.items():
-    payload = k + ' device=' + node_name + ' value=' + str(v) + ' ' + str(node_time)
-    print('payload',payload)
-
-    url = "http://sms-influxdb-container:8086/write?db="+node_db
-    r = requests.post(url, data=payload)
-    print(r.status_code)
-  #  response = requests.request("POST", url, headers= {'Content-Type': 'text/plain'}, data = payload)
-  #  print(response.code)
-  #  print(response.text.encode('utf8'))
+    print(k,v)
 
 def on_connect(client, userdata, flags, rc):
   print("error = "+str(rc))
