d3e3071f0d59f73d7adfa915666c9552e864b868
Ubuntu <sms-admin@sms-pallene.s0kf3j1octjedkiexudbontl3a.ax.internal.cloudapp.net>
2020-07-28 00:43:33 +0000

    divide script to run and build containers

--- sms-scripts/build-containers.sh
+++ sms-scripts/build-containers.sh
@@ -1,5 +1,5 @@
-# docker rm sms-node-emulator-container
-# docker build -t sms-node-emulator ../sms-node-emulator/
+docker rm sms-node-emulator-container
+docker build -t sms-node-emulator ../sms-node-emulator/
 
 docker run -d --name=sms-influxdb-container --network sms-net -v v-sms-influx:/var/lib/influxdb influxdb:1.7
 docker run -d -p 3000:3000 --name=sms-grafana-container --network sms-net -v v-sms-grafana:/var/lib/grafana grafana/grafana
