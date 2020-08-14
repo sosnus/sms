5dad695d2b27e3cf7e027bd22d8c9ccd00e66239
Sosnus <sosnus15@gmail.com>
2020-08-14 21:02:11 +0200

    clean code and change database name

---
+++ smsvariables.py
@@ -0,0 +1,12 @@
+# var for sms-app
+
+app_id = "arduino-mkr-1310-test"
+access_key = "ttn-account-v2.1yNpDkRAfRnEvrh2fwxO5a4IfrCszOyFEFwTj-DY8rQ"
+
+default_database = "greenhouse_jaroszki_14_08"
+#"orchard_jaroszki"
+default_url = "http://sms-pallene.westeurope.cloudapp.azure.com"
+default_port = 8086
+print("variables loaded")
+
+# endpoint_influxdb = "aaa"
