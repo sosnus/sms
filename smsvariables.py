a26aa774bd8c8dec87a2f384092b4e247d22e73c
Sosnus <sosnus15@gmail.com>
2020-08-14 21:41:25 +0200

    change influxDb hostname

--- smsvariables.py
+++ smsvariables.py
@@ -4,8 +4,10 @@ app_id = "arduino-mkr-1310-test"
 access_key = "ttn-account-v2.1yNpDkRAfRnEvrh2fwxO5a4IfrCszOyFEFwTj-DY8rQ"
 
 default_database = "greenhouse_jaroszki_14_08"
+default_database1 = "greenhouse_jaroszki"
 #"orchard_jaroszki"
-default_url = "http://sms-pallene.westeurope.cloudapp.azure.com"
+default_url = "http://sms-influxdb-container"
+default_url1 = "http://sms-pallene.westeurope.cloudapp.azure.com"
 default_port = 8086
 print("variables loaded")
 
