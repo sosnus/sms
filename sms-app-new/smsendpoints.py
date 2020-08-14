b59cf4940f48f725fe7022ef55d3be5cdd8d8182
Sosnus <sosnus15@gmail.com>
2020-08-14 20:35:30 +0200

    http request done, but without timer and python lib

--- sms-app-new/smsendpoints.py
+++ sms-app-new/smsendpoints.py
@@ -15,26 +15,32 @@ def init():
   print ("sms-endpoints init")
   
 def writeToInfluxDB(databasePort = smsvariables.default_port, databaseUrl =smsvariables.default_url ,databaseName = smsvariables.default_database , messageTime = datetime.now(), measurementParameter = "temporaryTemp", measurementValue = 42, measurementNodeName = "defaultNode"):
+    if False:
+        print("PARAMETERS:")
+        print(databasePort)
+        print(databaseUrl)
+        print(databaseName)
+        print(messageTime)
+        print(measurementParameter)
+        print(measurementValue)
+        print(measurementNodeName)
+        print("-------")
+       
+        print("all")
+        
+    payload = measurementParameter + ",device=" + measurementNodeName + " value="+ str(measurementValue)
     
-    print("PARAMETERS:")
-    print(databasePort)
-    print(databaseUrl)
-    print(databaseName)
-    print(messageTime)
-    print(measurementParameter)
-    print(measurementValue)
-    print(measurementNodeName)
-    print("-------")
-   
-    print("all")
-    payload = "k,device=" + measurementNodeName + " value="+ str(measurementValue)
+    headers = {
+    'Content-Type': 'text/plain'
+    }
     
-#    headers = {
-#    'Content-Type': 'text/plain'
-#    }
-    
+    url = databaseUrl + ":" + str(databasePort) + "/write?db="+databaseName
+
     print(payload)
-#    print(response.text.encode('utf8'))
+    print(url)
+    response = requests.request("POST", url, headers=headers, data = payload)
+    time.sleep(2)
+    print(response.text.encode('utf8'))
 
 
    ### response = requests.request("POST", url, headers=headers, data = payload)
