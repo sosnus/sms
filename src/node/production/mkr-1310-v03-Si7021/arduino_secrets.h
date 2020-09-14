d726f8a35354d96fe9f76bc04cbf28ae22ad174d
Sosnus <sosnus15@gmail.com>
2020-09-14 05:05:28 +0200

    Update arduino_secrets.h

--- src/node/production/mkr-1310-v03-Si7021/arduino_secrets.h
+++ src/node/production/mkr-1310-v03-Si7021/arduino_secrets.h
@@ -1,12 +1,13 @@
-// Application ID  sms-application
-// Device ID warehouse-1-floor
-//
-//// Replace with keys obtained from TheThingsNetwork console
-//#define SECRET_APP_EUI "70B3D57ED0033CEB"
-//#define SECRET_APP_KEY "C2C56EB6445FC250B5980E8BBDA4E656"
+// Application ID: sms-application
+// Device ID: greenhouse
+// Replace with keys obtained from TheThingsNetwork console
+#define SECRET_APP_EUI "70B3D57ED0033CEB"
+#define SECRET_APP_KEY "729D3430026DEC681DE06AE759CDB431"
+
+#define INTERVAL  300*1000
+// interval in milliseconds
+
 
 
 // z zieloną naklejką
 // GARAGE
-#define SECRET_APP_EUI "70B3D57ED0033CEB"
-#define SECRET_APP_KEY "729D3430026DEC681DE06AE759CDB431"
